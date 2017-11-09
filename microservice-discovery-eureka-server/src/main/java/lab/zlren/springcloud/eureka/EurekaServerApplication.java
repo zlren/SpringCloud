1b6afc5435359e8d28a8384308de2515784af1eb
zlren <zlren2012@163.com>
2017-11-09 23:01:17 +0800

    服务注册与服务发现

---
+++ microservice-discovery-eureka-server/src/main/java/lab/zlren/springcloud/eureka/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package lab.zlren.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka-server
+ *
+ * @author zlren
+ * @date 2017-11-07
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
