9688e97d10450f5cbdcdae3f1ba78ecef850ad6b
zlren <zlren2012@163.com>
2017-11-17 23:04:51 +0800

    使用 hystrix 实现容错处理

--- microservice-provider-user/src/main/java/lab/zlren/springcloud/user/UserApplication.java
+++ microservice-provider-user/src/main/java/lab/zlren/springcloud/user/UserApplication.java
@@ -2,12 +2,14 @@ package lab.zlren.springcloud.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author zlren
  * @date 2017-11-07
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
--- microservice-provider-user/src/main/java/lab/zlren/springcloud/user/controller/UserController.java
+++ microservice-provider-user/src/main/java/lab/zlren/springcloud/user/controller/UserController.java
@@ -1,12 +1,11 @@
 package lab.zlren.springcloud.user.controller;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import lab.zlren.springcloud.user.domain.User;
 import lab.zlren.springcloud.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author zlren
@@ -19,6 +18,9 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private EurekaClient eurekaClient;
+
     /**
      * 注：@GetMapping("/{id}")是spring 4.3的新注解等价于：
      *
@@ -30,5 +32,16 @@ public class UserController {
     @GetMapping("/{id}")
     public User findById(@PathVariable Long id) {
         return this.userRepository.findOne(id);
+    }
+
+    @GetMapping("eureka-instance")
+    public String serviceUrl() {
+        InstanceInfo instanceInfo = this.eurekaClient.getNextServerFromEureka("MICROSERVICE-PROVIDER-USER", false);
+        return instanceInfo.getHomePageUrl();
+    }
+
+    @PostMapping("")
+    public User postUser(@RequestBody User user) {
+        return user;
     }
 }
